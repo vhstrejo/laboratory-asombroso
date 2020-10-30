fde53811512a33cc852bbb27b4a3692ef50277dc
Fred Madrid <madbook@Freds-MacBook-Pro.local>
2020-10-30 15:29:30 -0700

    added activate and landingpage

---
+++ README.md
@@ -0,0 +1,7 @@
+# Asomboroso front-end interface
+
+| Resource | Description
+|-|-
+| [Contributing](https://github.com/Genobank/genobank.io/blob/master/CONTRIBUTING.md) | Notes to start and deploy this project.
+| [Documentation](https://github.com/Genobank/docs) | Overall project documentation.
+| [Admin](https://admin.genobank.io) | Repository with utils for generating tokens and secrets.
---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+
+
+
+var pageURL = $(location).attr("href");
+var biosampleId = null;
+parts = pageURL.split("?")[1].split("#");
+parts = parts[0].split("&");
+biosampleId = parts[0].split("=")[1];
+timestamp = parts[1].split("=")[1];
+signature = parts[2].split("=")[1];
+
+$checkOwner = async function () {
+    await fetch(`https://app.genobank.io/biosamples/`, {
+        method: 'GET',
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.owner) { // already exists
+            
+        } else { // continue
+             $('#invalidKit').modal('show');
+        }
+      })
+      .catch((error) => {
+        alert('Error:', error);
+        console.error('Error:', error);
+      });
+	}
+
+$(function () {
+
+$checkOwner();
+
+
+});
